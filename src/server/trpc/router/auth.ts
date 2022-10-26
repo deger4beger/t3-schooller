ff094804b256c196ba92680a9d159dc4686488ea
deger4beger <deger.begerrr@gmail.com>
2022-10-26 20:18:56 +0600

    feat: signin added

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -3,6 +3,7 @@ import * as bcrypt from "bcryptjs";
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 import { getSignedToken } from "../../../utils/jwt";
+import { TRPCError } from "@trpc/server";
 
 const userValidator = z.object({
 	id: z.number(),
@@ -30,7 +31,7 @@ const authRouter = router({
 				jwt: z.string(),
 			})
 		)
-		.query(async ({ input, ctx }) => {
+		.mutation(async ({ input, ctx }) => {
 			const cookies = new Cookies(ctx.req, ctx.res);
 			const userPayload = {
 				...input,
@@ -45,6 +46,53 @@ const authRouter = router({
 				data: { token: refreshToken, userId: userOutput.id },
 			});
 
+			cookies.set("refresh", refreshToken, {
+				httpOnly: true,
+				sameSite: true,
+			});
+			return {
+				userData: userOutput,
+				jwt: getSignedToken(userOutput),
+			};
+		}),
+	signin: publicProcedure
+		.input(
+			z.object({
+				email: z.string().email(),
+				password: z.string().min(8).max(20),
+			})
+		)
+		.output(
+			z.object({
+				userData: userValidator,
+				jwt: z.string(),
+			})
+		)
+		.mutation(async ({ input, ctx }) => {
+			const cookies = new Cookies(ctx.req, ctx.res);
+
+			const user = await ctx.prisma.user.findUnique({
+				where: { email: input.email },
+			});
+			if (!user) {
+				throw new TRPCError({ code: "NOT_FOUND" });
+			}
+
+			const passwordMatch = await bcrypt.compare(input.password, user.password);
+			if (!passwordMatch) {
+				throw new TRPCError({ code: "BAD_REQUEST" });
+			}
+			const { password, ...userOutput } = user;
+
+			const refreshToken = getSignedToken(userOutput, true);
+			ctx.prisma.refreshToken.update({
+				where: {
+					userId: userOutput.id,
+				},
+				data: {
+					token: refreshToken,
+				},
+			});
 			cookies.set("refresh", refreshToken, {
 				httpOnly: true,
 				sameSite: true,
