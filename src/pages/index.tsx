37b22c680d56d75ba8d39e6041ef214cfe60d227
deger4beger <deger.begerrr@gmail.com>
2022-10-28 22:13:14 +0600

    chore: latest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
+import PageShell from "../components/page-shell";
 
 const Home: NextPage = () => {
 
   return (
-    <>
-      Test comm
-    </>
+    <PageShell title="Home">
+      <h1 className="text-4xl">Home</h1>
+    </PageShell>
   );
 };
 
