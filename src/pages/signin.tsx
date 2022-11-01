c4dbcba7876ac87f5efd01a87a49ff6df1a5fcde
deger4beger <deger.begerrr@gmail.com>
2022-11-01 19:43:20 +0600

    init: auth pages

---
+++ src/pages/signin.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Auth: NextPage = () => {
+	return (
+		<div>
+			signin page
+		</div>
+	)
+}
+
+export default Auth
---
+++ src/pages/signup.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Auth: NextPage = () => {
+	return (
+		<div>
+			signup page
+		</div>
+	)
+}
+
+export default Auth
