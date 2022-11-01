506eca243b18cafebeea5112aac87a91a37c46ea
deger4beger <deger.begerrr@gmail.com>
2022-11-01 19:57:51 +0600

    init: session context

---
+++ src/hooks/session-context.ts
@@ -0,0 +1,8 @@
+import { createContext, useContext } from "react";
+import { AppRouterTypes } from "../utils/trpc"
+
+type AuthOutput = AppRouterTypes["auth"]["signup"]["output"]
+
+export const SessionContext = createContext<AuthOutput | null>(null)
+
+export const useSession = () => useContext(SessionContext)
