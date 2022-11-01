46870cb2d0c1ce0e8a9c1aedfed1aa631f42fdf2
deger4beger <deger.begerrr@gmail.com>
2022-11-01 19:43:31 +0600

    init: client routes

---
+++ src/utils/routes.ts
@@ -0,0 +1,11 @@
+export const privateRoutes: string[][] = [
+	["Домой", "/"],
+	["О нас", "/about"],
+	["Контакты", "/contacts"],
+	["Профиль", "/profile"],
+]
+
+export const publicRoutes: string[][] = [
+	["Вход", "/signin"],
+	["Регистрация", "/signup"],
+]
