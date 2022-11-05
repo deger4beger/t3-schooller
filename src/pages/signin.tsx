03bff9d5b5fb4178c745a4fdf1d4f118bc77db25
deger4beger <deger.begerrr@gmail.com>
2022-11-05 13:40:15 +0600

    rearrange: input component removed

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,11 +1,10 @@
 import { NextPage } from "next"
-import Input from "../components/input"
 import PageShell from "../components/page-shell"
 
 const Auth: NextPage = () => {
 	return (
 		<PageShell title="Вход">
-			<Input type="text" className="rounded" />
+			vxod
 		</PageShell>
 	)
 }
