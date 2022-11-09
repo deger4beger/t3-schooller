2fa0c466cb5447431d4e6c39c78c963b7e58e8c5
deger4beger <deger.begerrr@gmail.com>
2022-11-09 20:04:15 +0600

    chore: todo's removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
 import type { NextPage } from "next";
 import PageShell from "../components/page-shell";
 
-// ## TODO: choose one
-// ## TODO: add radix-ui
-// ## TODO: add headless-ui
-// ## TODO: add evergreen-ui
-// ## TODO: add other ui-library
 const Home: NextPage = () => {
   return (
     <PageShell title="Дом">
