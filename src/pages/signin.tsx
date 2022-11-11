a5e458dfea61898bbbe2fd30d71de8f10855ce8d
deger4beger <deger.begerrr@gmail.com>
2022-11-11 20:11:53 +0600

    feat: some progress

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -4,7 +4,12 @@ import PageShell from "../components/page-shell"
 const Auth: NextPage = () => {
 	return (
 		<PageShell title="Вход">
-			vxod
+			<div className="p-4 bg-zinc-300 rounded border flex flex-col border-zinc-700">
+				<input className="m-4 h-10 bg-zinc-200 p-2 border rounded border-4 border-zinc-600" />
+				<input className="m-4 h-10 bg-zinc-200 p-2 border rounded border-[1px] border-zinc-600" />
+				<input className="m-4 h-10 bg-zinc-200 p-2 border rounded border-[1px] border-zinc-400" />
+				<input className="m-4 h-10 bg-zinc-200 p-2 border rounded border-[1px] border-zinc-400" />
+			</div>
 		</PageShell>
 	)
 }
