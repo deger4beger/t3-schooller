b3d82fa47ecd863852664be24eeb7b347e0e5b77
deger4beger <deger.begerrr@gmail.com>
2022-12-07 20:24:06 +0600

    feat: session provider added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@
 import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { SessionContext } from "../hooks/session-context";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <SessionContext.Provider value={ null }>
+    <Component {...pageProps} />
+  </SessionContext.Provider>;
 };
 
 export default trpc.withTRPC(MyApp);
